9d47fd21d5760c7df8dc67a8aa6fffa9b5fe58ab
ValeriyKobysh <vmksocnet@outlook.com>
2018-02-13 11:39:26 +0200

    GHP

--- dist/js/article.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([8],{76:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a=n(77),i=(n.n(a),n(6),n(0)),c=n.n(i),l=(n(7),n(1));let o=new c.a({el:"#article",mixins:[l.a],data:{},methods:{}});t.default=o},77:function(e,t){}},[76]);
